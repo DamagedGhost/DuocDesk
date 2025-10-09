3ad0998675d6b6297ab51684f662d0b082f6b81a
DamagedGhost <felipevasquez977@gmail.com>
2025-10-08 22:17:30 -0300

    Rollback a java 21, prueba de proyecto java

--- bk_DuocDesk/src/main/java/com/example/demo/DuocDeskApplication.java
+++ bk_DuocDesk/src/main/java/com/example/demo/DuocDeskApplication.java
@@ -8,6 +8,8 @@ public class DuocDeskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DuocDeskApplication.class, args);
-	}
 
+		System.out.println("Servicio iniciado correctamente");
+		System.out.println("Esperando solicitudes...");
+	}
 }
